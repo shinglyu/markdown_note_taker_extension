a70a87d113ed282df076914bc4d30c877f988234
Shing Lyu <shing.lyu@gmail.com>
2021-10-29 11:54:38 +0200

    Added copy page link and copy selected as title for link

--- src/background.js
+++ src/background.js
@@ -7,24 +7,64 @@ function copyToClipboard( text ){
   document.execCommand("Copy", false, null);
   document.body.removeChild(textarea);
 }
+
 chrome.contextMenus.create({
   id: "selected_text",
-  title: "Copy selected as markdown note",
+  title: "Copy selected as markdown quote",
   contexts: ["selection"]
 });
 
+chrome.contextMenus.create({
+  id: "selected_text_as_link",
+  title: "Copy selected as title for markdown link",
+  contexts: ["selection"]
+});
+
+chrome.contextMenus.create({
+  id: "page_link",
+  title: "Copy page title as markdown link",
+  contexts: ["all"]
+});
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  let text = "Something went wrong with Markdown Note Taker Extension"
   switch (info.menuItemId) {
     case "selected_text":
       console.debug(info.selectionText);
-      const text = `> ${info.selectionText} (Reference: [${tab.title}](${tab.url}))`
+      text = `> ${info.selectionText} (Reference: [${tab.title}](${tab.url}))`
       copyToClipboard(text)
-      //alert(text)
       chrome.notifications.create(
         "selected_text_copied",
         {
           title: "Markdown note copied",
-          message: "Selected text copied to clipboard as markdown note",
+          message: "Selected text copied to clipboard as markdown quote",
+          type: "basic",
+          iconUrl: "../icon_placeholder.png"
+        }
+      )
+      break;
+    case "selected_text_as_link":
+      console.debug(info.selectionText);
+      text = `[${info.selectionText}](${tab.url})`
+      copyToClipboard(text)
+      chrome.notifications.create(
+        "selected_text_copied",
+        {
+          title: "Markdown note copied",
+          message: "Selected text copied to clipboard as markdown link",
+          type: "basic",
+          iconUrl: "../icon_placeholder.png"
+        }
+      )
+      break;
+    case "page_link":
+      text = `[${tab.title}](${tab.url})`
+      copyToClipboard(text)
+      chrome.notifications.create(
+        "selected_text_copied",
+        {
+          title: "Markdown note copied",
+          message: "Page link copied to clipboard as markdown link",
           type: "basic",
           iconUrl: "../icon_placeholder.png"
         }
